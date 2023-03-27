fb05330e87b87defd33692bf07c0c3bf0b87b54c
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2023-03-27 17:32:08 -0300

    dists

--- src/models/qp_database.go
+++ src/models/qp_database.go
@@ -150,7 +150,11 @@ func Migrations(fullPath string) (migrations []migrate.SqlxMigration) {
 	log.Debugf("migrating files from: %s", fullPath)
 	files, err := ioutil.ReadDir(fullPath)
 	if err != nil {
-		log.Fatal(err)
+		if strings.Contains(err.Error(), "cannot find the file specified") {
+			log.Warnf("no migrations found at: %s", fullPath)
+		} else {
+			log.Fatal(err)
+		}
 	}
 
 	log.Debug("migrating creating array with definitions")
@@ -182,6 +186,8 @@ func Migrations(fullPath string) (migrations []migrate.SqlxMigration) {
 		}
 	}
 
+	migrations = append(migrations, GetBase())
+
 	for _, migration := range confMap {
 		migrations = append(migrations, migration.ToSqlxMigration())
 	}
@@ -192,6 +198,45 @@ func Migrations(fullPath string) (migrations []migrate.SqlxMigration) {
 	})
 
 	return
+}
+
+// Provides the first migration
+func GetBase() migrate.SqlxMigration {
+	migration := migrate.SqlxQueryMigration("1", `
+	CREATE TABLE IF NOT EXISTS "users" (
+		"username" CHAR (255) PRIMARY KEY NOT NULL,
+		"password" VARCHAR (255) NOT NULL,
+		"timestamp" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+	  );
+	  
+	  CREATE TABLE IF NOT EXISTS "servers" (
+		"token" CHAR (100) PRIMARY KEY UNIQUE NOT NULL,
+		"wid" VARCHAR (255) UNIQUE NOT NULL,
+		"verified" BOOLEAN NOT NULL DEFAULT FALSE,
+		"devel" BOOLEAN NOT NULL DEFAULT FALSE,
+		"handlegroups" BOOLEAN NOT NULL DEFAULT TRUE,
+		"handlebroadcast" BOOLEAN NOT NULL DEFAULT FALSE,
+		"user" CHAR (36) DEFAULT NULL REFERENCES "users"("username"),
+		"timestamp" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+	  );
+	  
+	  CREATE TABLE IF NOT EXISTS "webhooks" (
+		"context" CHAR (255) NOT NULL REFERENCES "servers"("token"),
+		"url" VARCHAR (255) NOT NULL,
+		"forwardinternal" BOOLEAN NOT NULL DEFAULT FALSE,
+		"trackid" VARCHAR (100) NOT NULL DEFAULT '',
+		"extra" BLOB DEFAULT NULL,
+		"timestamp" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+		CONSTRAINT "webhooks_pkey" PRIMARY KEY ("context", "url")
+	  );
+	  
+	  INSERT OR REPLACE INTO migrations (id) VALUES
+	  ('202207131700'),
+	  ('202209281840'),
+	  ('202303011900')
+	  ;
+	  `, "")
+	return migration
 }
 
 type QpMigrator struct {
